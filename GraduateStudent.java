a5f3ac97751e5b1b895e0ba51b94f2f63573dace
refal11fofo <refal.alhrbi.111@gmail.com>
2026-05-21 00:38:22 +0300

    Add files via upload

---
+++ GraduateStudent.java
@@ -0,0 +1,29 @@
+package com.mycompany.studentdatamanager;
+
+public class GraduateStudent extends Student {
+
+    private static final long serialVersionUID = 2L;
+    private final String researchTopic;
+    private final String supervisor;
+
+    public GraduateStudent(String name, String studentId, double gpa,
+            String department, String email, String phone,
+            String researchTopic, String supervisor) {
+        super(name, studentId, gpa, department, email, phone);
+        this.researchTopic = researchTopic;
+        this.supervisor = supervisor;
+    }
+
+    public String getResearchTopic() {
+        return researchTopic;
+    }
+
+    public String getSupervisor() {
+        return supervisor;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Research: " + researchTopic + " | Supervisor: " + supervisor;
+    }
+}
---
+++ Student.java
@@ -0,0 +1,66 @@
+package com.mycompany.studentdatamanager;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+private static final long serialVersionUID = 1L;
+    private String name;
+    private String studentId;
+    private double gpa;
+    private String department;
+    private ContactInfo contactInfo;
+
+    public Student(String name, String studentId, double gpa, String department, String email, String phone) {
+        this.name = name;
+        this.studentId = studentId;
+        this.gpa = gpa;
+        this.department = department;
+        this.contactInfo = new ContactInfo(email, phone);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public ContactInfo getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(ContactInfo contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + studentId + " | Name: " + name + " | GPA: " + gpa
+                + " | Dept: " + department + " | " + contactInfo.toString();
+    }
+}
